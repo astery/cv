15445d47f1dbf642041a28ec8f32672eac43c66c
Andrey Akulov <andreyshark@gmail.com>
2025-05-09 16:09:22 +0300

    Fix missing icon set

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 // Vite will properly treat font resources in these
-import "@fortawesome/fontawesome-free/css/fontawesome.css";
-import "@fortawesome/fontawesome-free/css/brands.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 import "./app.css";
